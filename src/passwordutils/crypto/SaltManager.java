81e66abbf1422a8bf0fbaeec3367a2e3920a0a27
collantes04 <joseantonio.sanchez.collantes.alu@iesfernandoaguilar.es>
2026-01-26 12:30:15 +0100

    preview 5-v1.0

--- src/passwordutils/crypto/SaltManager.java
+++ src/passwordutils/crypto/SaltManager.java
@@ -1,5 +1,15 @@
 package passwordutils.crypto;
 
+import java.security.SecureRandom;
+
 public class SaltManager {
-    
+    protected final SecureRandom sr = new SecureRandom();
+
+    protected byte[] generateSalt(int len) {
+        byte bytesArr[] = new byte[len];
+        
+        sr.nextBytes(bytesArr);
+
+        return bytesArr;
+    }
 }
